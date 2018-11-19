07c3667b028a714a5575a12e0978acbac9e85819
Yann SAN SEBASTIAN <ysan-seb@e3r1p9.42.fr>
2018-11-19 19:25:10 +0100

    Grace.c

---
+++ Grace.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+# define FORMAT	"#include <stdio.h>%c#include <fcntl.h>%c%c# define FORMAT%c%c%s%c%c# define PARAM%cFORMAT,10,10,10,9,34,FORMAT,34,10,9,10,9,34,34,10,10%c# define MAIN%cint main(void) { int file; file = open(%cGrace_kid.c%c, O_CREAT | O_RDWR, 0644); dprintf(file, PARAM); return (0);}%c%cMAIN;"
+# define PARAM	FORMAT,10,10,10,9,34,FORMAT,34,10,9,10,9,34,34,10,10
+# define MAIN	int main(void) { int file; file = open("Grace_kid.c", O_CREAT | O_RDWR, 0644); dprintf(file, PARAM); return (0);}
+
+MAIN;
