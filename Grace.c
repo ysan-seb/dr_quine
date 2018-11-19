70de212b5dfcf1d646792befeeddd83f4dfcdd35
Yann SAN SEBASTIAN <ysan-seb@e3r1p9.42.fr>
2018-11-19 19:57:32 +0100

    add comment

--- Grace.c
+++ Grace.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-# define FORMAT	"#include <stdio.h>%c#include <fcntl.h>%c%c# define FORMAT%c%c%s%c%c# define PARAM%cFORMAT,10,10,10,9,34,FORMAT,34,10,9,10,9,34,34,10,10,10,10,10,10%c# define MAIN%cint main(void) { int file; file = open(%cGrace_kid.c%c, O_CREAT | O_RDWR, 0644); dprintf(file, PARAM); return (0);}%c%c/*%c** Comment%c*/%c%cMAIN;"
-# define PARAM	FORMAT,10,10,10,9,34,FORMAT,34,10,9,10,9,34,34,10,10,10,10,10,10
+# define FORMAT	"#include <stdio.h>%c#include <fcntl.h>%c%c# define FORMAT%c%c%s%c%c# define PARAM%cFORMAT,10,10,10,9,34,FORMAT,34,10,9,10,9,34,34,10,10,10,9,10,10,10%c# define MAIN%cint main(void) { int file; file = open(%cGrace_kid.c%c, O_CREAT | O_RDWR, 0644); dprintf(file, PARAM); return (0);}%c%c/*%c%cComment%c*/%c%cMAIN;"
+# define PARAM	FORMAT,10,10,10,9,34,FORMAT,34,10,9,10,9,34,34,10,10,10,9,10,10,10
 # define MAIN	int main(void) { int file; file = open("Grace_kid.c", O_CREAT | O_RDWR, 0644); dprintf(file, PARAM); return (0);}
 
 /*
-** Comment
+	Comment
 */
 
 MAIN;
